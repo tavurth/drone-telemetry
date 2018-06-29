e2d044eb1faa18b419381f0c34432546a02577a2
will <will.whitty.arbeit@gmail.com>
2018-06-30 01:16:26 +0300

    Added a button to change the drone configuration on the fly

--- frontend/src/Components/Admin/index.js
+++ frontend/src/Components/Admin/index.js
@@ -1,21 +1,38 @@
 import React from 'react';
 
 import Tabs from 'components/Tabs';
+import websocket from 'utils/SocketWrapper';
+
+import styles from './styles.scss';
 
 class AdminPanel extends Tabs {
+    sendConfig = () => {
+        websocket.emit('changeConfig', {
+            data: 'test-1',
+            id: 'drone-configuration',
+        });
+    };
+
     getTabsConfig() {
         return [
             {
-                title: 'Information',
-                component: () => <span>General information to go here</span>,
-            },
-            {
                 title: 'Controls',
-                component: () => <span>Maybe drone controls here</span>,
+                component: () => (
+                    <div className={styles.admin__tab}>
+                        <span>Maybe drone controls here</span>
+                        <a onClick={this.sendConfig} className={styles.button__update}>
+                            Update drone settings
+                        </a>
+                    </div>
+                ),
             },
             {
                 title: 'Table setup',
                 component: () => <span>Table or other setup here</span>,
+            },
+            {
+                title: 'Information',
+                component: () => <span>General information to go here</span>,
             },
         ];
     }
